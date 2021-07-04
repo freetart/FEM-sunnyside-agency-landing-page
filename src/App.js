f33c788ea219d9e608a2f080e3c7821aa385c6ed
habib <habibdev96@gmail.com>
2021-07-04 02:14:56 -0600

    created tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
+import Tag from './components/Tag';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Gallery />
       </main>
       <Footer />
+      <Tag />
     </>
   );
 }
---
+++ src/components/Tag.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { flexCenter, textStyles, headingStyles } from '../abstracts/Mixins';
+import logo from '../images/logo/habibdevgif.gif';
+
+const Article = styled.article`
+  ${flexCenter};
+  flex-wrap: wrap;
+  padding: 2rem 2rem 1rem 2rem;
+  background-color: var(--black);
+
+  .tag-heading {
+    ${textStyles};
+    font-size: var(--sizeXs);
+    color: var(--white);
+  }
+
+  .tag-link {
+    ${headingStyles};
+    color: var(--teal);
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+
+  .tag-logo {
+    width: 5rem;
+  }
+`;
+
+const Tag = () => {
+  return (
+    <Article>
+      <h3 className='tag-heading'>
+        project for{' '}
+        <a
+          href='https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP'
+          target='_blank'
+          rel='noreferrer'
+          className='tag-link'
+        >
+          frontend mentor
+        </a>{' '}
+        coded by{' '}
+        <a
+          href='https://github.com/habibdev96'
+          target='_blank'
+          rel='noreferrer'
+          className='tag-link'
+        >
+          habibdev{' '}
+        </a>
+      </h3>
+      <img src={logo} alt='logo' className='tag-logo' />
+    </Article>
+  );
+};
+
+export default Tag;
